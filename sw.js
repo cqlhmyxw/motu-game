858edcbe3610d41c64d29f9b5f40af7363e8eb8b
Hermes <hermes@nousresearch.com>
2026-09-11 05:30:47 +0000

    P218: 竹竿质感升级(渐变+节环+高光) 金币立体铜钱 墨珠径向渐变 符纸宣纸朱砂

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='motu-v10';
+const CACHE='motu-v11';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-72.png','./icon-96.png','./icon-128.png','./icon-144.png','./icon-152.png','./icon-192.png','./icon-384.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
